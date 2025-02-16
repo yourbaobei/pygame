97f8319fb864983cbceb4eda2d053312fed7b0ea
yourbaobei <sonyasemspb@gmail.com>
2025-02-16 14:07:58 +0300

    5

--- main.py
+++ main.py
@@ -3,13 +3,31 @@ import random
 pg.init()
 FPS = 30
 clock = pg.time.Clock()
+
 x = random.randint(1, 500)
 y = random.randint(1, 500)
-win = pg.display.setmode((500, 500))
+
+win = pg.display.set_mode((500, 500))
 win.fill('white')
+x = 200
+speed = -5
+
+"""
 for i in range(100):
-    pg.draw.circle(win, (112, 204, 209), (x, y), 50)
+
+    x = random.randint(1, 500)
+    y = random.randint(1, 500)
+    a = random.randint(1, 250)
+    b = random.randint(1, 250)
+    c = random.randint(1, 250)
+    pg.draw.circle(win, (a, b, c), (x, y), 30)
+"""
 while True:
+    x = x + speed
+    if x >= 600 or x <= 0:
+        speed = -1 * speed
+    win.fill('white')
+    pg.draw.circle(win, (0, 0, 0), (x, 200), 30)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
